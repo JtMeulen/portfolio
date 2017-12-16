b08561703102bb87946c4f85f0597f40e819127c
JoeyM <joeytermeulen@gmail.com>
2017-12-16 22:59:06 +0100

    Add new page Other Work with all the styling and images, also add route to app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-// Required modules
+// required modules
 const 	express = require("express"),
 		app		= express();
 
+// configuration
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + "/public"));
 
+// homepage
 app.get("/", function(req, res){
 	res.render("index");
 });
 
+// project pages
 app.get("/shoppingcard", function(req, res){
 	res.render("shoppingcard");
 });
@@ -18,10 +21,16 @@ app.get("/mountaintravel", function(req, res){
 	res.render("mountaintravel");
 });
 
+app.get("/otherwork", function(req, res){
+	res.render("other_work");
+});
+
+// external website
 app.get("/mountaintravel/example", function(req, res){
 	res.render("mountaintravel_example");
 });
 
+// server setup
 app.listen(app.get('port'), function(){
 	console.log("Server started on port" + app.get('port'));
 });
